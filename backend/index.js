1e681781ece73436b91ba7e0015975ed60e9db8c
Yash Biradar <ykkho833@gmail.com>
2025-10-26 02:30:54 +0530

    Add waving emoji to backend greeting message

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
-// Import the Express library
-const express = require('express');
 
-// Create our app
+const express = require('express');
+const cors = require('cors'); 
+
+
 const app = express();
+app.use(cors()); 
 const PORT = process.env.PORT || 3000;
 
-// This tells our app to listen for "GET" requests on the main "/" URL
 app.get('/', (req, res) => {
-  res.send('Hello from the Waste Management Backend! ');
+  res.send('Hello from the Waste Management Backend! 👋');
 });
 
-// Start the server and listen for connections on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
